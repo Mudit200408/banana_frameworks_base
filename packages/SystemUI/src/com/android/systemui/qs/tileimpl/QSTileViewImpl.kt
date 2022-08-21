5e53c48214d2c02590d9c35c5b4b416a25ec4508
timjosten <timjosten@gmail.com>
2022-08-21 07:40:03 +0000

    SystemUI: Improve vertical QS tile layout & animation
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -221,7 +221,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
         vertical = QSLayoutUtils.getQSTileVerticalLayout(context)
         if (isA11Style) {
             orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
+            gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
         } else {
             orientation = if (vertical) LinearLayout.VERTICAL else LinearLayout.HORIZONTAL
             gravity = if (vertical) Gravity.CENTER_HORIZONTAL else Gravity.CENTER_VERTICAL or Gravity.START  
@@ -311,7 +311,7 @@ open class QSTileViewImpl @JvmOverloads constructor(
         vertical = QSLayoutUtils.getQSTileVerticalLayout(context)
         if (isA11Style) {
             orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
+            gravity = Gravity.CENTER_HORIZONTAL or Gravity.CENTER_VERTICAL
         } else {
             orientation = if (vertical) LinearLayout.VERTICAL else LinearLayout.HORIZONTAL
             gravity = if (vertical) Gravity.CENTER_HORIZONTAL else Gravity.CENTER_VERTICAL or Gravity.START  
@@ -430,7 +430,8 @@ open class QSTileViewImpl @JvmOverloads constructor(
         // is too short.
         val constrainedSquishiness = constrainSquishiness(squishinessFraction)
         bottom = top + (actualHeight * constrainedSquishiness).toInt()
-        scrollY = (actualHeight - height) / 2
+        scrollY = (actualHeight - height) / if (vertical) 7 else 2
+        label.alpha = if (!vertical) 1.0f else Math.pow(squishinessFraction.toDouble(), 7.0).toFloat()
     }
 
     override fun updateAccessibilityOrder(previousView: View?): View {
